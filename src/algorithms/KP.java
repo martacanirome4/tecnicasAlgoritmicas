b91922391efc235a101db758b94e5d48ee112d56
MartaCanino <martacaninoromero@gmail.com>
2023-05-12 09:32:35 +0200

    Makefile y manifest

---
+++ src/algorithms/KP.java
@@ -0,0 +1,14 @@
+package algorithms;
+
+// The Knapsack Problem (KP) (problema de la mochila)
+public class KP {
+
+    /**
+     * Disponemos de n objetos de valores vi(i:1...n) y pesos pi(i:1...n) respectivamente
+     * Queremos cargar una mochila con una coleccion de esos objetos que maximice la suma
+     * de los valores transportados
+     * La capacidad maxima de la mochila es M ≥ 0
+     */
+
+
+}
---
+++ src/algorithms/MED.java
@@ -0,0 +1,7 @@
+package algorithms;
+
+// Minimum Edit Distance -- Dan Jurafsky (Stanford University, 2009)
+public class MED {
+
+
+}
