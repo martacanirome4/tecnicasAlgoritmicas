b303a7e85587fa4e27fc17d1f3ae3ea75aabd9f8
MartaCanino <martacaninoromero@gmail.com>
2023-05-24 17:51:02 +0200

    MED problem

--- src/algorithms/MED.java
+++ src/algorithms/MED.java
@@ -1,7 +1,50 @@
 package algorithms;
+import java.util.*;
 
 // Minimum Edit Distance -- Dan Jurafsky (Stanford University, 2009)
 public class MED {
+
+    // space optimization
+
+    /**
+     * El método utiliza programación dinámica para calcular la cantidad mínima de operaciones (inserciones,
+     * eliminaciones y sustituciones) necesarias para convertir una cadena en otra.
+     *
+     * Utiliza dos matrices para almacenar las distancias de edición mínimas para diferentes subproblemas e itera sobre
+     * los caracteres de las cadenas para calcular la solución óptima.
+     *
+     * @param s: cadena 1 a comparar
+     * @param t: cadena 2 a comparar
+     * @return la cantidad mínima de operaciones necesarias para convertir una cadena en otra
+     */
+    public int editDistance(String s, String t) {
+        int n = s.length();
+        int m = t.length();
+
+        int[] prev = new int[m + 1];
+        int[] curr = new int[m + 1];
+
+        for (int j = 0; j <= m; j++) {
+            prev[j] = j;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            curr[0] = i;
+            for (int j = 1; j <= m; j++) {
+                if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                    curr[j] = prev[j - 1];
+                }
+                else {
+                    int mn = Math.min(1 + prev[j],
+                            1 + curr[j - 1]);
+                    curr[j] = Math.min(mn, 1 + prev[j - 1]);
+                }
+            }
+            prev = curr.clone();
+        }
+
+        return prev[m];
+    }
 
 
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,11 @@
 package main;
 import algorithms.KP;
+import algorithms.MED;
 
 public class Main {
     public static void main(String[] args) {
+
+        // KP algorithm
         int[] weights = {2, 3, 4, 5};
         int[] values = {3, 4, 5, 6};
         int numItems = 4;
@@ -10,6 +13,15 @@ public class Main {
 
         int maxValue = KP.knapsackDP(weights, values, numItems, capacity);
         System.out.println("Maximum value: " + maxValue);
+
+        // MED algorithm
+        String s = "marta";
+        String t = "marciano";
+
+        MED ob = new MED();
+
+        int ans = ob.editDistance(s, t);
+        System.out.println("The total number of operations to convert "+ s +" to "+ t +" is: " + ans);
     }
 
 }
